63ecdfbdb9113dc211942b9bfc533a4f62ec69a3
KaoHu <kaohulogist@gmail.com>
2022-07-19 00:18:34 -0500

    text edit

--- myapp.py
+++ myapp.py
@@ -7,16 +7,16 @@ from sklearn.ensemble import RandomForestClassifier
 st.write("""
 # Simple Iris Flower Prediction App
 
-This app predicts the Iris flower type!
+This app predicts the **Iris flower** type!
 """)
 
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    sepal_length = st.sidebar.slider('sepal_length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('sepal_width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('petal_length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('petal_width', 0.1, 2.5, 0.2)
+    sepal_length = st.sidebar.slider('Sepal Length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal Length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal Width', 0.1, 2.5, 0.2)
     data = {'sepal_length': sepal_length,
             'sepal_width': sepal_width,
             'petal_length': petal_length,
